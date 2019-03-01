23e5a8eeaf48e8ec94745c9fabb8c7a708e38b13
W3R10S <47816763+W3R10S@users.noreply.github.com>
2019-03-01 09:46:07 +0100

    Réseau
    Personnages liés

---
+++ Project/Assets/ProjetS4/Personnage/Scripts/PlayerObject.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+        //Instantiate(PlayerUnitPrefab);
+        //NetworkServer.Spawn(PlayerUnitPrefab);
+        CmdSpawnMyUnit();
+    }
+
+
+    public GameObject PlayerUnitPrefab;
+
+    [SyncVar]
+    public string PlayerName = "Anonymous";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdMoveUnitUp();
+        }*/
+
+        /*if ( Input.GetKeyDown(KeyCode.Q) )
+        {
+            string n = "Quill" + Random.Range(1, 100);
+
+            Debug.Log("Sending the server a request to change our name to : " + n);
+            CmdChangePlayerName(n);
+        }*/
+    }
+
+    GameObject myPlayerUnit;
+
+    [Command]
+    void CmdSpawnMyUnit()
+    {
+        GameObject go = Instantiate(PlayerUnitPrefab);
+
+        myPlayerUnit = go;
+
+        //go.GetComponent<NetworkIdentity>().AssignClientAuthority(connectionToClient);
+
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+    }
+
+    /*[Command]
+    void CmdMoveUnitUp()
+    {
+        if (myPlayerUnit == null)
+        {
+            return;
+        }
+
+
+        myPlayerUnit.transform.Translate(0, 1, 0);
+    }*/
+
+    [Command]
+    void CmdChangePlayerName(string n)
+    {
+        Debug.Log("CmdChangePlayerName : " + n);
+        PlayerName = n;
+
+       // RpcChangePlayerName(PlayerName);
+    }
+
+    /*[ClientRpc]
+    void RpcChangePlayerName(string n)
+    {
+        Debug.Log("RpcChangePlayerName : We were asked to change the player name on a particular PlayerObject : " + n);
+        PlayerName = n;
+    }*/
+}
